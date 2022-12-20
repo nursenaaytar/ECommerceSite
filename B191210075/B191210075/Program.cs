b186d6fb0fe95283a86a60bfe0fa07746684527a
nursenaaytar <nursena.aytar@gmail.com>
2022-12-20 19:50:42 +0300

    update,delete, insert düzenlemesi yapıldı

--- B191210075/B191210075/Program.cs
+++ B191210075/B191210075/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=AdminMenu}/{action=Index}/{id?}");
+    pattern: "{controller=ProductCategory}/{action=Index}/{id?}");
 
 app.Run();
