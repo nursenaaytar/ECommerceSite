5f011320c9aad2faee2048b96fe0b71b948092a9
nursenaaytar <nursena.aytar@gmail.com>
2022-12-23 22:24:27 +0300

    order model

---
+++ B191210075/B191210075/Models/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceSite.Models
+{
+    public class OrderViewModel
+    {
+        public int? SqId { get; set; }
+        public string? Creater { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public string? Modifier { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
